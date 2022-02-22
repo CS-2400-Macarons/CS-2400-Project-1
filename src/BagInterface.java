44ce20ca4bab2405af2dfec2a036eb5c27431cbe
Phung Ly <98497119+pkly1219@users.noreply.github.com>
2022-02-22 13:05:38 -0800

    Add intersection description

--- src/BagInterface.java
+++ src/BagInterface.java
@@ -33,6 +33,11 @@ public interface BagInterface<T>
     public T[] toArray();
 
     public T[] union(T[] bag);
+    
+    /** Finds the similarity elements appearing in the first bag and the second bag.
+       @param bag the bag to compare with the first bag.
+       @return a new allocated array of all the similarity of the two bags
+    */
     public T[] intersection(T[] bag);
 
     /** Finds the difference between the first bag to the second bag.
